4a94a584f8d4fd1d7a263690fbfcb067f41de5ee
Iamneo <techadmin@iamneo.ai>
2023-09-21 11:16:50 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -5,14 +5,14 @@ function validate()
             var che = check(password)
             if(uname.length ==0)
             {
-            document.getElementById("ans").innerHTML = "Please enter both username and password.";
+            document.getElementById("result").innerHTML = "Please enter both username and password.";
             }
             else if(che == "false")
             {
-                document.getElementById("ans").innerHTML = "Invalid username or password.";    
+                document.getElementById("result").innerHTML = "Invalid username or password.";    
             }
             else{
-                document.getElementById("ans").innerHTML = "Login Successful.";
+                document.getElementById("result").innerHTML = "Login successful.";
             }
         }
 function check(password)
